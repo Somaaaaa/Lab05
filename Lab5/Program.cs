8e1dd868ef24fbdde115613e3e25083edb7c4b73
Rille Soma <rillesoma75@gmail.com>
2023-10-13 19:11:56 +0200

    upload

---
+++ Lab5/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+
+namespace Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1. feladat
+            /*
+            StreamReader myReader = new StreamReader("colorem_ipsum.txt", System.Text.Encoding.ASCII);
+            while (!myReader.EndOfStream)
+            {
+                string oneLine = myReader.ReadLine();
+                string[] a = oneLine.Split('#');
+                ConsoleColor color = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), a[0]);
+                Console.ForegroundColor = color;
+                Console.WriteLine(a[1]);
+            }
+            myReader.Close();
+            */
+            #endregion
+            #region 2. feladat
+            /*
+            StreamWriter myWriter = new StreamWriter("lotto.txt", true);
+            Random rnd = new Random();
+            List<int> numbers = new List<int>();
+            string answer;
+            int szam;
+            DateTime today = DateTime.Now;
+            do
+            {
+                numbers.Clear();
+                for (int i = 0; i < 5; i++)
+                {
+                    do
+                    {
+                        szam = rnd.Next(1, 91);
+
+                    } while (numbers.Contains(szam) == true);
+                    numbers.Add(szam);
+                }
+                Console.WriteLine("On {0} numbers were {1} {2} {3} {4} {5}", today.ToShortDateString(), numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]);
+                myWriter.WriteLine("On {0} numbers were {1} {2} {3} {4} {5}", today.ToShortDateString(), numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]);
+                Console.Write("Another week? [y/n] ");
+                today = today.AddDays(7);
+                answer = Console.ReadLine();
+            } while (answer != "n");
+            myWriter.Close();
+            */
+            #endregion
+            #region 4. feladat
+            /*
+            string[] allLines = File.ReadAllLines("NHANES_1999-2018.csv");
+
+            string[] SURVEY = new string[allLines.Length-1];
+            int[] RIAGENDR = new int[allLines.Length-1];
+            int[] RIDAGEYR = new int[allLines.Length-1];
+            double[] BMXBMI = new double[allLines.Length-1];
+            double[] LBDGLUSI = new double[allLines.Length-1];
+            for(int i = 0; i < allLines.Length; i++)
+            {
+                string[] a = allLines[i+1].Split(',');
+                SURVEY[i] = a[1];
+                RIAGENDR[i] = int.Parse(a[2]);
+                RIDAGEYR[i] = int.Parse(a[3]);
+                BMXBMI[i] = double.Parse(a[4]);
+                LBDGLUSI[i] = double.Parse(a[5]);
+            }
+            */
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
